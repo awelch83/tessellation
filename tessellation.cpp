55ae1a4935eff945f42cb07f47adc61ac41f8575
Aaron Welch <acw036@Aarons-MacBook-Pro.local>
2024-05-12 16:20:52 -0500

    Polygons created as expected. Tested / works in inkscape

--- tessellation.cpp
+++ tessellation.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <cstring>
 #include <iostream>
 #include <string>
 
@@ -7,6 +8,7 @@ using namespace std;
 float getDistance(float,float,float,float);
 float getX(float,float,float,float,float);
 float getY(float,float,float,float,float);
+float currentDist;
 
 int main()
 {
@@ -14,7 +16,6 @@ int main()
     int numVertices;
     float pixMove;
     float x1,y1,x2,y2;
-    string polygon = "<polygon points=\"";
 
     cout << "Vertices: ";
     cin >> numVertices;
@@ -32,37 +33,44 @@ int main()
         cin >> curVertices[i][1];
     }
 
-    for(int j = 0; j < numVertices; j++)
+    do
     {
-        if((j+1) < numVertices)
+        string polygon = "<polygon points=\"";
+
+        for(int j = 0; j < numVertices; j++)
         {
-            x1 = curVertices[j][0];
-            y1 = curVertices[j][1];
-            x2 = curVertices[j+1][0];
-            y2 = curVertices[j+1][1];
+            if((j+1) < numVertices)
+            {
+                x1 = curVertices[j][0];
+                y1 = curVertices[j][1];
+                x2 = curVertices[j+1][0];
+                y2 = curVertices[j+1][1];
+            }
+            else
+            {
+                x1 = curVertices[j][0];
+                y1 = curVertices[j][1];
+                x2 = curVertices[0][0];
+                y2 = curVertices[0][1];
+            }
+            nextVertices[j][0] = getX(x1,y1,x2,y2,pixMove);
+            nextVertices[j][1] = getY(x1,y1,x2,y2,pixMove);
+
+            polygon += to_string(nextVertices[j][0]);
+            polygon += ",";
+            polygon += to_string(nextVertices[j][1]);
+            if((j+1) < numVertices)
+                polygon += " ";
+            else
+                polygon += "\" ";
         }
-        else
-        {
-            x1 = curVertices[j][0];
-            y1 = curVertices[j][1];
-            x2 = curVertices[0][0];
-            y2 = curVertices[0][1];
-        }
-        nextVertices[j][0] = getX(x1,y1,x2,y2,pixMove);
-        nextVertices[j][1] = getY(x1,y1,x2,y2,pixMove);
 
-        polygon += to_string(nextVertices[j][0]);
-        polygon += ",";
-        polygon += to_string(nextVertices[j][1]);
-        if((j+1) < numVertices)
-            polygon += " ";
-        else
-            polygon += "\" ";
-    }
+        polygon += " stroke=\"white\" fill=\"none\" stroke-width=\"1\"/>";
+        cout << polygon << endl;
 
-    polygon += " stroke=\"white\" fill=\"none\" stroke-width=\"1\"/>";
+        memcpy(curVertices,nextVertices,sizeof(nextVertices));
 
-    cout << polygon << endl;
+    } while (currentDist >= pixMove);
 
     return 0;
 }
@@ -70,6 +78,7 @@ int main()
 float getDistance(float x1, float y1, float x2, float y2)
 {
     float d = sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+    currentDist = d;
 
     return d;
 }
