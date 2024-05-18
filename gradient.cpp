73258f5036ec382896f5ade9a8462eac79b01f2b
Aaron Welch <acw036@Aarons-MacBook-Pro.local>
2024-05-18 14:20:56 -0500

    Accept color inputs

--- gradient.cpp
+++ gradient.cpp
@@ -4,8 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Starting Color (hex): "
-    cout << "Ending Color (hex): "
+    char start_r, start_g, start_b, end_r, end_g, end_b;
+
+    cout << "Starting Color (R): "
+    cin >> start_r;
+    cout << "Starting Color (G): "
+    cin >> start_g;
+    cout << "Starting Color (B): "
+    cin >> start_b;
+    cout << "  Ending Color (R): "
+    cin >> end_r;
+    cout << "  Ending Color (G): "
+    cin >> end_g;
+    cout << "  Ending Color (B): "
+    cin >> end_b;
 
     return 0;
 }
