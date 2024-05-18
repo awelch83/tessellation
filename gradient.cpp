f31724449c4a8e7809dc0279f61cc44c5040948d
Aaron Welch <acw036@Aarons-MacBook-Pro.local>
2024-05-18 14:22:19 -0500

    Syntax correction

--- gradient.cpp
+++ gradient.cpp
@@ -6,17 +6,17 @@ int main()
 {
     char start_r, start_g, start_b, end_r, end_g, end_b;
 
-    cout << "Starting Color (R): "
+    cout << "Starting Color (R): ";
     cin >> start_r;
-    cout << "Starting Color (G): "
+    cout << "Starting Color (G): ";
     cin >> start_g;
-    cout << "Starting Color (B): "
+    cout << "Starting Color (B): ";
     cin >> start_b;
-    cout << "  Ending Color (R): "
+    cout << "  Ending Color (R): ";
     cin >> end_r;
-    cout << "  Ending Color (G): "
+    cout << "  Ending Color (G): ";
     cin >> end_g;
-    cout << "  Ending Color (B): "
+    cout << "  Ending Color (B): ";
     cin >> end_b;
 
     return 0;
